53bbdb6d271e542ff839be792f730210c344c437
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-10-08 22:25:23 +0530

    [Feat]: Added CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("/api/v1/ships", require("./src/routes/ship"));
