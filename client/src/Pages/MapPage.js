96fd8428acfa9868a4e6f0f1f122da53a3317341
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-10-08 23:29:12 +0530

    [Feat]: Created a polygon input for taking input of different shapes

--- client/src/Pages/MapPage.js
+++ client/src/Pages/MapPage.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 // Leaflet
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  FeatureGroup,
+} from "react-leaflet";
 import leaflet from "leaflet";
+import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
+import "leaflet-draw/dist/leaflet.draw.css";
 
 // Marker Function
 const markerIcon = new leaflet.Icon({
@@ -13,12 +21,23 @@ const markerIcon = new leaflet.Icon({
   popupAnchor: [0, -46],
 });
 
+// Polygon create function and options
+const _created = (e) => console.log(e);
+const polygonOptions = {
+  rectangle: false,
+  circle: false,
+  circlemarker: false,
+  marker: false,
+  polyline: false,
+};
+
 const MapPage = () => {
   // States
   // const [center, setCenter] = useState([13.084622, 80.248357]);
   const [center, setCenter] = useState([-38.233562, 178.554214]);
   const [ships, setShips] = useState([]);
-  const [zoomLevel, setZoomLevel] = useState(9);
+  const [zoomLevel, setZoomLevel] = useState(7);
+  const mapRef = useRef();
 
   const fetchData = async () => {
     const res = await fetch("http://localhost:5000/api/v1/ships");
@@ -31,7 +50,14 @@ const MapPage = () => {
   }, []);
   return (
     <div>
-      <MapContainer center={center} zoom={zoomLevel}>
+      <MapContainer center={center} zoom={zoomLevel} ref={mapRef}>
+        <FeatureGroup>
+          <EditControl
+            position="topright"
+            onCreated={_created}
+            draw={polygonOptions}
+          />
+        </FeatureGroup>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
