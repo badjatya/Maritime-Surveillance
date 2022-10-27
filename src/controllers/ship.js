43919f8022e7d0b739575f6f324461a1ab752609
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-10-28 02:36:45 +0530

    [Feat]: Filtering data based on polygon input

--- src/controllers/ship.js
+++ src/controllers/ship.js
@@ -1,5 +1,7 @@
 // Importing Models
 const AIS = require("../models/ais");
+const insidePolygon = require("../utils/insidePolygon");
+const aggregatePipeline = require("../utils/aggregatePipeline");
 
 exports.getShipById = async (req, res) => {
   try {
@@ -34,54 +36,93 @@ exports.getShipByShipId = async (req, res) => {
   }
 };
 
-exports.getShipsByPolygonInput = async (req, res) => {
+// exports.getShipsByPolygonInput = async (req, res) => {
+//   try {
+//     const body = req.body[0][0];
+//     const length = body.length;
+//     let latitude = 0;
+//     let longitude = 0;
+
+//     for (let i = 0; i < body.length; i++) {
+//       latitude += body[i].lat;
+//       longitude += body[i].lng;
+//     }
+
+//     const startLat = latitude / length + 1;
+//     const startLng = longitude / length;
+//     const endLat = startLat > 0 ? startLat + 1 : startLat - 1;
+//     const endLng = startLng > 0 ? startLng + 1 : startLng - 1;
+
+//     const aisData = await AIS.find({
+//       Latitude: { $gte: endLat, $lte: startLat },
+//       Longitude: { $gte: startLng, $lte: endLng },
+//     });
+
+//     console.log("Start Latitude: " + startLat);
+//     console.log("End Latitude: " + endLat);
+//     console.log("Start Longitude: " + startLng);
+//     console.log("End Longitude: " + endLng);
+
+//     res.status(200).json({
+//       status: "success",
+//       aisData,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       status: "error",
+//       error: "Internal server error",
+//       message: "Please try again",
+//     });
+//   }
+// };
+
+exports.getShips = async (req, res) => {
   try {
-    const body = req.body[0][0];
-    const length = body.length;
-    let latitude = 0;
-    let longitude = 0;
-
-    for (let i = 0; i < body.length; i++) {
-      latitude += body[i].lat;
-      longitude += body[i].lng;
-    }
-
-    const startLat = latitude / length + 1;
-    const startLng = longitude / length;
-    const endLat = startLat > 0 ? startLat + 1 : startLat - 1;
-    const endLng = startLng > 0 ? startLng + 1 : startLng - 1;
-
-    const aisData = await AIS.find({
-      Latitude: { $gte: endLat, $lte: startLat },
-      Longitude: { $gte: startLng, $lte: endLng },
-    });
-
-    console.log("Start Latitude: " + startLat);
-    console.log("End Latitude: " + endLat);
-    console.log("Start Longitude: " + startLng);
-    console.log("End Longitude: " + endLng);
-
+    const aisData = await AIS.aggregate(aggregatePipeline).limit(200).exec();
     res.status(200).json({
       status: "success",
-      aisData,
+      results: aisData.length,
+      data: aisData,
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       status: "error",
-      error: "Internal server error",
       message: "Please try again",
     });
   }
 };
 
-exports.getShips = async (req, res) => {
+exports.getShipsByPolygonInput = async (req, res) => {
   try {
-    const distinctShips = await AIS.find({}).distinct("Ship_ID");
-    const data = await AIS.find({ Ship_ID: { $in: distinctShips } }).limit(20);
+    const body = req.body[0][0];
+    let polygon = [];
+    let data = [];
+
+    // Arranging polygon data
+    body.map((data) => {
+      let temp = [];
+      temp.push(data.lat);
+      temp.push(data.lng);
+      polygon.push(temp);
+    });
+
+    // Fetching data
+    const aisData = await AIS.aggregate(aggregatePipeline).exec();
+
+    aisData.map((d) => {
+      let point = [];
+      point.push(d.Latitude);
+      point.push(d.Longitude);
+      const result = insidePolygon(point, polygon);
+      if (result) {
+        data.push(d);
+      }
+    });
+
     res.status(200).json({
       status: "success",
-      results: data.length,
-      data,
+      aisData: data,
     });
   } catch (error) {
     res.status(500).json({
